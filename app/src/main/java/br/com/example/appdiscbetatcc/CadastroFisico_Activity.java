8ba412016e21e4de63c9882bf7a69abc4df6c004
Rodrigo Miklos Vieira <rodrigo.vieira67@etec.sp.gov.br>
2019-05-12 10:04:35 -0300

    Feito PopUp Cadastro e iniciado tela login P.Fisica

--- app/src/main/java/br/com/example/appdiscbetatcc/CadastroFisico_Activity.java
+++ app/src/main/java/br/com/example/appdiscbetatcc/CadastroFisico_Activity.java
@@ -2,17 +2,12 @@ package br.com.example.appdiscbetatcc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CadastroFisico_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void ShowPopup(View view) {
-
+        setContentView(R.layout.cadastro_fisico_layout);
     }
 }
--- app/src/main/java/br/com/example/appdiscbetatcc/ShowPopup_ctivity.java
+++ app/src/main/java/br/com/example/appdiscbetatcc/ShowPopup_ctivity.java
@@ -2,17 +2,12 @@ package br.com.example.appdiscbetatcc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ShowPopup_ctivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void ShowPopup(View view) {
-
+        setContentView(R.layout.showpopup_layout);
     }
 }
