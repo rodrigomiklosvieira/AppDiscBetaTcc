b8af640b4f08ed33846c60791e43650d4bd5438c
Rodrigo Miklos Vieira <rodrigo.vieira67@etec.sp.gov.br>
2019-05-13 16:19:25 -0300

    Dando funcionalidade aos botõos do PopUp

--- app/src/main/java/br/com/example/appdiscbetatcc/ShowPopup_ctivity.java
+++ app/src/main/java/br/com/example/appdiscbetatcc/ShowPopup_ctivity.java
@@ -2,12 +2,42 @@ package br.com.example.appdiscbetatcc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class ShowPopup_ctivity extends AppCompatActivity {
+
+    Button PJuridica;
+    Button PFisica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.showpopup_layout);
+
+        PJuridica = (Button) findViewById(R.id.btnJuridico);
+        PFisica = (Button) findViewById(R.id.btnFisica);
+
+    }
+
+    public void setPFisica(Button PFisica) {
+        Intent intent = new Intent(ShowPopup_ctivity.this, CadastroFisico_Activity.class);
+        startActivity(intent);
+
+    }
+
+    public void setPJuridica(Button PJuridica) {
+        Intent intent = new Intent(ShowPopup_ctivity.this, CadastroJuridico_Activity.class);
+        startActivity(intent);
+
+        }
+
     }
 }
+
