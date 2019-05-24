32370ebd9a7912dd3c42024b5dff7a3b837d4010
Rodrigo Miklos Vieira <rodrigo.vieira67@etec.sp.gov.br>
2019-05-24 15:35:31 -0300

    Finalizado a tela de Login e inicializado a tela do teste.

---
+++ app/src/main/java/br/com/example/appdiscbetatcc/Descricao_Activity.java
@@ -0,0 +1,13 @@
+package br.com.example.appdiscbetatcc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Descricao_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.descricao_layout);
+    }
+}
---
+++ app/src/main/java/br/com/example/appdiscbetatcc/Teste_Activity.java
@@ -0,0 +1,13 @@
+package br.com.example.appdiscbetatcc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Teste_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teste_layout);
+    }
+}
