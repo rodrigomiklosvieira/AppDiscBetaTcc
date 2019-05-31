4ffb25972f11e856c105bff16f0c2a32a27dc459
Rodrigo Miklos Vieira <rodrigo.vieira67@etec.sp.gov.br>
2019-05-31 08:54:49 -0300

    Começado a inclusão dos ids no app

--- app/src/main/java/br/com/example/appdiscbetatcc/MainActivity.java
+++ app/src/main/java/br/com/example/appdiscbetatcc/MainActivity.java
@@ -4,18 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sairSistema;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sairSistema = (Button)findViewById(R.id.btnSair);
     }
 
     public void ShowPopup(View view) {
         Intent intent = new Intent(MainActivity.this, ShowPopup_ctivity.class);
         startActivity(intent);
+
+    }
+
+    public void Login(View view) {
+        Intent intent = new Intent(MainActivity.this, Login_Activity.class);
+        startActivity(intent);
+    }
+
+    public void Sair(View view) {
 
     }
 }
