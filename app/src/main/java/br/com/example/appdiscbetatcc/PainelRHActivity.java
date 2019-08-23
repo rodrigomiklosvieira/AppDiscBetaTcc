c076294cbe0f633940165b31664dea7c5136c23a
rodrigomiklosvieira <rodrigo.vieira67@etec.sp.gov.br>
2019-08-23 20:15:58 -0300

    Implementando botão spinner no painel do RH

--- app/src/main/java/br/com/example/appdiscbetatcc/PainelRHActivity.java
+++ app/src/main/java/br/com/example/appdiscbetatcc/PainelRHActivity.java
@@ -3,14 +3,50 @@ package br.com.example.appdiscbetatcc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import static android.R.layout.simple_spinner_item;
 
 public class PainelRHActivity extends AppCompatActivity {
-
+    Spinner selecionadados;
+    private ArrayList<String> names = new ArrayList<String>();
+    private ArrayList<spinner> goodModelArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_painel_rh);
+
+        selecionadados = (Spinner) findViewById(R.id.selecionadados);
+
+
+
+
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(PainelRHActivity.this, simple_spinner_item, names);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+        selecionadados.setAdapter(spinnerArrayAdapter);
+
+
+        selecionadados.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+    });
+
+
     }
+
+
 
     public void Cancelar(View view) {
         finish();
