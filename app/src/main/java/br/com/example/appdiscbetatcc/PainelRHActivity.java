bf3fe8dfa1bb860928c6b4e2d49021aa8e040849
carlosfloresta <carlos.floresta@etec.sp.gov.br>
2019-09-05 20:59:52 -0300

    popup criado para rh selecionar opções do candidato

--- app/src/main/java/br/com/example/appdiscbetatcc/PainelRHActivity.java
+++ app/src/main/java/br/com/example/appdiscbetatcc/PainelRHActivity.java
@@ -1,8 +1,10 @@
 package br.com.example.appdiscbetatcc;
 
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +17,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -45,10 +48,6 @@ public class PainelRHActivity extends AppCompatActivity {
     Spinner selecionadados;
     private ArrayList<String> names = new ArrayList<String>();
     private ArrayList<spinner> goodModelArrayList;
-
-
-
-
     StringRequest stringRequest;
     RequestQueue requestQueue;
     String nomeempresa;
@@ -58,59 +57,49 @@ public class PainelRHActivity extends AppCompatActivity {
     String itemSelecionado;
     ListView LstPainel;
     String id_empresa;
-    String nome,telefone,email;
+    String nome, telefone, email;
     String teste;
     int posicao;
     EditText eText;
-
     List<candidato> candidatoList;
-
     ArrayList<String> pesquisa = new ArrayList<String>();
-
-
-
-
-
+    Dialog dialog;
     String urlwebservices = "https://carlos.cf/apiRest/buscacandidato.php";
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_painel_rh);
-
         requestQueue = Volley.newRequestQueue(this);
-
-
-        nomeempresaa = (TextView)findViewById(R.id.textorh);
+        nomeempresaa = (TextView) findViewById(R.id.textorh);
         Intent intent = getIntent();
         String nomeemp = intent.getStringExtra("empresalogin");
-        id_empresa= intent.getStringExtra("empresaidlogin");
+        id_empresa = intent.getStringExtra("empresaidlogin");
         nomeempresaa.setText("Bem-Vindo(a), " + nomeemp + ".");
-        LstPainel = (ListView)findViewById(R.id.LstPainel);
+        LstPainel = (ListView) findViewById(R.id.LstPainel);
         candidatoList = new ArrayList<>();
         eText = (EditText) findViewById(R.id.eText);
-
+        dialog = new Dialog(this);
 
 
         LstPainel.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(),ListarDadosCandidatos.class);
-                final candidato cand = candidatoList.get(position);
-                intent.putExtra("name",cand.getId());
+            //    Intent intent = new Intent(getApplicationContext(), ListarDadosCandidatos.class);
+               // final candidato cand = candidatoList.get(position);
+              // intent.putExtra("name", cand.getId());
+                test();
 
-                startActivity(intent);
+               // startActivity(intent);
             }
         });
 
 
         spinner = (Spinner) findViewById(R.id.selecionadados);
-        lista = new ArrayList<>(Arrays.asList("Nome","E-mail","Telefone"));
+        lista = new ArrayList<>(Arrays.asList("Nome", "E-mail", "Telefone"));
 
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, lista );
+                android.R.layout.simple_spinner_item, lista);
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(dataAdapter);
@@ -127,9 +116,8 @@ public class PainelRHActivity extends AppCompatActivity {
                 itemSelecionado = lista.get(posicao);
 
 
-
-               CandidatoAdapter adapter = new CandidatoAdapter(candidatoList);
-             LstPainel.setAdapter(adapter);
+                CandidatoAdapter adapter = new CandidatoAdapter(candidatoList);
+                LstPainel.setAdapter(adapter);
 
             }
 
@@ -142,7 +130,7 @@ public class PainelRHActivity extends AppCompatActivity {
         });
 
 
-      //  Pesquisar();
+        Pesquisar();
 
         eText.addTextChangedListener(new TextWatcher() {
 
@@ -157,9 +145,29 @@ public class PainelRHActivity extends AppCompatActivity {
 
             public void onTextChanged(CharSequence arg0, int arg1, int arg2,
                                       int arg3) {
-        //        Pesquisar();
+                Pesquisar();
 
                 LstPainel.setAdapter(new ArrayAdapter<String>(PainelRHActivity.this, android.R.layout.simple_list_item_1, pesquisa));
+            }
+        });
+
+        selecionaCandidato();
+    }
+
+    public void test(){
+        Button PJuridica;
+        Button PFisica;
+        TextView txtclose;
+        dialog.setContentView(R.layout.popup_rh_escolha);
+
+        txtclose =(TextView) dialog.findViewById(R.id.txtclose);
+
+
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v2) {
+
+                dialog.dismiss();
             }
         });
 
@@ -167,46 +175,40 @@ public class PainelRHActivity extends AppCompatActivity {
 
 
 
-
-        selecionaCandidato();
-
-
-
-
-
-
-
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.show();
     }
 
 
- /*   public void Pesquisar() {
+
+
+
+
+    public void Pesquisar() {
         int textlength = eText.getText().length();
+        int sequenceLength = 0;
+        Integer sequenceInteger = null;
         pesquisa.clear();
 
 
+        for (int i = 0; i < candidatoList.size(); i++) {
+            final candidato cand = candidatoList.get(i);
+            if (candidatoList.get(i).equals(cand.getId())) {
 
-        for (int i = 0; i < candidato.length; i++) {
-            if (textlength <= lstFrutas[i].length()) {
-                if (eText.getText().toString().equalsIgnoreCase((String) lstFrutas[i].subSequence(0, textlength))) {
-                    pesquisa.add(lstFrutas[i]);
+                sequenceLength++;
+
+                if ((Boolean) eText.getText().toString().equalsIgnoreCase(String.valueOf(false))) {
+                    pesquisa.add(String.valueOf(candidatoList.get(i)));
                 }
             }
         }
-
     }
-*/
-
-
 
     public void Cancelar(View view) {
         Intent intent = new Intent(PainelRHActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
-
-
-
-
 
     private void selecionaCandidato() {
 
@@ -228,11 +230,9 @@ public class PainelRHActivity extends AppCompatActivity {
                                 candidatoList.clear();
 
 
-
                                 JSONArray dataArray = jsonObject.getJSONArray("data");
 
                                 for (int i = 0; i < dataArray.length(); i++) {
-
 
 
                                     JSONObject obj = dataArray.getJSONObject(i);
@@ -307,15 +307,15 @@ public class PainelRHActivity extends AppCompatActivity {
             final candidato cand = candidatoList.get(position);
 
 
-            if(idposicao  == 0) {
+            if (idposicao == 0) {
 
                 textViewName.setText(cand.getNome());
 
-            }else if(idposicao == 1){
+            } else if (idposicao == 1) {
 
                 textViewName.setText(cand.getEmail());
 
-            }else if(idposicao  == 2){
+            } else if (idposicao == 2) {
 
                 textViewName.setText(cand.getTelefone());
             }
@@ -325,5 +325,4 @@ public class PainelRHActivity extends AppCompatActivity {
     }
 
 
-
-    }
+}
